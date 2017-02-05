6fc5c855e0625b29aaf010fc0d9625c8c380c43d
Christophe THEPAUT <christophe@thepaut.net>
2017-02-05 08:07:37 +0100

    initial commit

---
+++ src/main/java/com/bnpparibas/services/helloworld/HelloWorldConfiguration.java
@@ -0,0 +1,39 @@
+package com.bnpparibas.services.helloworld;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.NotEmpty;
+import javax.validation.constraints.*;
+
+public class HelloWorldConfiguration extends Configuration {
+    
+	@NotEmpty
+	@JsonProperty
+	private String template;
+	
+	@NotEmpty
+	@JsonProperty
+	private String defaultName = "Stranger";
+	
+	@JsonProperty
+	public String getTemplate() {
+		return template;
+	}
+	
+	@JsonProperty
+	public void setTemplate(String template) {
+		this.template = template;
+	}
+	
+	@JsonProperty
+	public String getDefaultName() {
+		return defaultName;
+	}
+	
+	@JsonProperty 
+	public void setDefaultName(String defaultName) {
+		this.defaultName = defaultName;
+	}
+	
+	
+}
